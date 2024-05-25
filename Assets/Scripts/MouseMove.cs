dca52aaa3723fffe3bd83decc4f5e89984f124b4
claire601 <clacha24@bergen.org>
2024-05-24 20:18:17 -0400

    applied movement to mouse
    ok ig when you commit the scene itself does not save (?) so i just dragged n dropped everything if it didn't work tell me!

--- Assets/Scripts/MouseMove.cs
+++ Assets/Scripts/MouseMove.cs
@@ -4,15 +4,158 @@ using UnityEngine;
 
 public class MouseMove : MonoBehaviour
 {
+
+    public GameObject player;
+    Rigidbody2D rb;
+    Animator anim;
+    float moveForce = 2;
+    private bool isGrounded;
+    private bool onLadder;
+    public float jumpTimeCounter;
+    public float jumpTime;
+    public float airTime;
+    private bool isJumping;
+
+     void OnCollisionEnter2D(Collision2D col){
+        if (col.gameObject.tag == ("Ground"))
+        {
+            isGrounded = true;
+            Debug.Log("on ground");
+        }   
+
+        if (col.gameObject.tag == ("Ladder")){
+            onLadder = true;
+            Debug.Log("on ladder");
+        }
+      
+    }
+    void OnCollisionExit2D(Collision2D col){
+          if (col.gameObject.tag == ("Ground")){
+            isGrounded = false;
+            Debug.Log("off ground");
+          }
+
+          if (col.gameObject.tag == ("Ladder")){
+            onLadder = false;
+            Debug.Log("off ladder");
+          }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 position = transform.position;
+
+      if (Input.GetKey(KeyCode.RightArrow)){
+      rb.AddForce(transform.right * (-moveForce), ForceMode2D.Force); 
+      anim.SetBool("isMovingRight", true);
+      anim.SetBool("isMoving", false);
+      anim.SetBool("isIdleRight", false);
+      }
+      else{
+        anim.SetBool("isMovingRight", false);
+      }
+      
+      if (Input.GetKey(KeyCode.LeftArrow)) {
+      rb.AddForce(-transform.right * (-moveForce), ForceMode2D.Force); 
+      anim.SetBool("isMoving", true);
+      anim.SetBool("isMovingRight", false);
+      anim.SetBool("isIdle", false);
+      }
+      else {
+        anim.SetBool("isMoving", false);
+      }
+
+      if (anim.GetBool("isIdleRight")){
+        Debug.Log("right");
+      }
+      else {
+        Debug.Log("no right");
+      }
+
+      if (anim.GetBool("isIdleRight") || anim.GetBool("isMovingRight")){
+          if (isGrounded == true && Input.GetKeyDown(KeyCode.Space)){
+          isJumping = true;
+          anim.SetBool("isJumpingRight", true);
+          jumpTimeCounter = jumpTime;
+          rb.velocity = Vector2.up * 5;
+
+      }
+
+      if (Input.GetKey(KeyCode.Space) && isJumping == true) {
+          if (jumpTimeCounter > 0){
+          rb.velocity = Vector2.up * 5;
+          anim.SetBool("isJumpingRight", true);
+          //rb.AddForce(transform.up * moveForce, ForceMode2D.Impulse); 
+          jumpTimeCounter -= Time.deltaTime;
+          }
+          else {
+            isJumping = false;
+          }
+      }
+      
+      if (Input.GetKeyUp(KeyCode.Space)){
+        isJumping = false;
+        anim.SetBool("isJumpingRight", false);
+      }
+      }
+
+
+      if (anim.GetBool("isIdle") || anim.GetBool("isMoving")){
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space)){
+          isJumping = true;
+          anim.SetBool("isJumping", true);
+          jumpTimeCounter = jumpTime;
+          rb.velocity = Vector2.up * 5;
+
+      }
+
+      if (Input.GetKey(KeyCode.Space) && isJumping == true){
+          if (jumpTimeCounter > 0){
+          rb.velocity = Vector2.up * 5;
+          anim.SetBool("isJumping", true);
+          //rb.AddForce(transform.up * moveForce, ForceMode2D.Impulse); 
+          jumpTimeCounter -= Time.deltaTime; 
+          } 
+          else {
+            isJumping = false;
+          }
+      }
+      
+      if (Input.GetKeyUp(KeyCode.Space)){
+        isJumping = false;
+        anim.SetBool("isJumping", false);
+      }
+      }
+
+      if (onLadder && Input.GetKey(KeyCode.UpArrow)){
+            anim.SetBool("isClimbing", true);
+            rb.AddForce(transform.up * (moveForce), ForceMode2D.Force);
+        }
+      
+      else if (!onLadder){
+        anim.SetBool("isClimbing", false);
+      }
+
+      
+      
+      if (position.x >= 7){
+          Debug.Log("out of bounds");
+          position.x = 7;
+          transform.position = position;
+      }
+
+      if (position.x <= -7){
+          Debug.Log("out of bounds");
+          position.x = -7;
+          transform.position = position;
+      }
     }
 }
+
