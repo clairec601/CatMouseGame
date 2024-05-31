5d62b17ab432627129dc6443455797877bb91fce
Khashiya <kharan24@bergen.org>
2024-05-31 10:04:03 -0400

    Fixed ScoreScript

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScoreScript : MonoBehaviour
 {
@@ -9,17 +10,17 @@ public class ScoreScript : MonoBehaviour
     
 
     public static int scoreValue = 0;
-    Text score;
+    public TextMeshProUGUI scoreText;
     // Start is called before the first frame update
     void Start()
     {
-        score = GetComponent<Text>();
+        scoreText = GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        score.text = "Score: " + scoreValue;
+        scoreText.text = "Score: " + scoreValue;
         
     }
 }
